d523469561ebc7a420e9a9bef7f5ea91196e1679
PosseVenture <info@posseventure.com>
2026-03-25 10:27:32 +0000

    Clean next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // This allows production builds to succeed even if there are TypeScript errors.
-    // We can remove this later once all types are fixed.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
